4bbc0c73349a2a4332f49e074ff0c3e4432a4a8a
Linuxthecoder <lynxthecoder@gmail.com>
2025-02-27 21:14:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -60,4 +60,4 @@ app.post("/submit", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));  // Fixed string interpolation
